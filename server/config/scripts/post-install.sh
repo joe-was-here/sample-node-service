b78926f4ea6cc8ad3543957c12b27d047d02327f
Joe Madden <joe.madden@gopetplan.com>
2019-10-01 16:51:23 -0400

    chore(git): set up husky pre push hook
    This will help enforce our git commit policy

--- server/config/scripts/post-install.sh
+++ server/config/scripts/post-install.sh
@@ -3,5 +3,12 @@
 # Set the git message template
 # git config --local commit.template ./config/.gitmessage
 current="$(pwd)"
-git config --local commit.template $current/config/.gitmessage
-echo $current
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo setting local commit template to $current/config/.gitmessage
+    git config --local commit.template $current/config/.gitmessage
+else
+    echo looks like this is not a mac, not running anything
+fi
+
+exit 0

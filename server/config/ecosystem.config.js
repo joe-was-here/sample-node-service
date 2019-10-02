bace1c49d6167fe5214247e2fbb3f5080159fbe0
Joe Madden <joe.madden@gopetplan.com>
2019-10-02 16:13:38 -0400

    feat(docker): dockerize the boilerplate template
    Next steps will be to try running a few of these on mini
    kube.  Idea is to make spinning up new services as quick
    and painless as possible for new devs.

--- server/config/ecosystem.config.js
+++ server/config/ecosystem.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apps : [
+  apps: [
     {
       name: 'API',
       script: 'dist/index.js',
@@ -9,11 +9,11 @@ module.exports = {
       watch: true,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
-      }
+        NODE_ENV: 'production',
+      },
     },
     {
       name: 'typescript watcher',
@@ -22,22 +22,11 @@ module.exports = {
       autorestart: true,
       watch: 'tsconfig.json',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
-      }
-    }
+        NODE_ENV: 'production',
+      },
+    },
   ],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
